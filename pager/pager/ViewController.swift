0a4ccf6f0fc4395445e43dd308338d965a850b7e
邓江洲 <dengjiangzhouReal@gmail.com>
2022-07-17 16:36:30 +0800

    one

--- pager/pager/ViewController.swift
+++ pager/pager/ViewController.swift
@@ -9,17 +9,34 @@ import UIKit
 
 
 
-class ViewController: UIPageViewController {
+class ViewController: UIViewController {
 
     lazy var left = ContentCtrl(isLeft: true)
     lazy var right = ContentCtrl(isLeft: false)
     lazy var greenView = UIView(frame: CGRect(x: 125, y: 250, width: 100, height: 100))
     
+    
+    lazy var pagedController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: UIPageViewController.NavigationOrientation.horizontal, options: [:])
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        dataSource = self
-        setViewControllers([left], direction: UIPageViewController.NavigationDirection.forward, animated: false, completion: nil)
+        addChild(pagedController)
+        pagedController.view.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(pagedController.view)
+        NSLayoutConstraint.activate([
+            pagedController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            pagedController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            pagedController.view.topAnchor.constraint(equalTo: view.topAnchor),
+            pagedController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
+        
+        
+        pagedController.dataSource = self
+        pagedController.setViewControllers([left], direction: UIPageViewController.NavigationDirection.forward, animated: false, completion: nil)
         
         greenView.backgroundColor = UIColor.green
         view.addSubview(greenView)
